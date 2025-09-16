3267477fa458b8838e3e22aa5fffd4b2c7f9ebf5
Sazid AL Emon <md.sazid.al.emon@gmail.com>
2025-09-16 14:50:21 +0600

    solved leetcode problem 1

---
+++ problem1.java
@@ -0,0 +1,60 @@
+/*
+* problem statement:
+*
+* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+* */
+
+
+//SOLUTION
+
+class Solution(object):
+def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+//Store numbers with their original indices
+        nums_with_index = [(num, i) for i, num in enumerate(nums)]
+                nums_with_index.sort()  # sort by number
+
+        left, right = 0, len(nums) - 1
+                while left < right:
+        curr_sum = nums_with_index[left][0] + nums_with_index[right][0]
+                if curr_sum == target:
+                return [nums_with_index[left][1], nums_with_index[right][1]]
+        elif curr_sum < target:
+        left += 1
+                else:
+        right -= 1
