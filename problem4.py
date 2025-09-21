2c04071503b08013c3baa373510263a3d7ae3a95
Sazid AL Emon <md.sazid.al.emon@gmail.com>
2025-09-21 13:13:27 +0600

    code solve

---
+++ problem4.py
@@ -0,0 +1,66 @@
+"""
+4. Median of Two Sorted Arrays
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+"""
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+        low, high = 0, m
+        total_len = m + n
+        half = (total_len + 1) // 2
+
+        while low <= high:
+            partition1 = (low + high) // 2
+            partition2 = half - partition1
+
+            maxLeft1 = float('-inf') if partition1 == 0 else nums1[partition1 - 1]
+            minRight1 = float('inf') if partition1 == m else nums1[partition1]
+
+            maxLeft2 = float('-inf') if partition2 == 0 else nums2[partition2 - 1]
+            minRight2 = float('inf') if partition2 == n else nums2[partition2]
+
+            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
+                if total_len % 2 == 1:
+                    return max(maxLeft1, maxLeft2)
+                else:
+                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0
+            elif maxLeft1 > minRight2:
+                high = partition1 - 1
+            else:
+                low = partition1 + 1
+
