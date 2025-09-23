adc098f075690cc714ef96784dd07ddfe47b5893
Sazid AL Emon <md.sazid.al.emon@gmail.com>
2025-09-23 09:08:02 +0600

    problem6 solution

---
+++ problem6.py
@@ -0,0 +1,69 @@
+"""
+Zigzag Conversion
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+"""
+
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        # Create rows
+        rows = [''] * numRows
+        current_row = 0
+        going_down = False
+
+        for char in s:
+            # Add character to current row
+            rows[current_row] += char
+
+            # Change direction if we're at top or bottom row
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+
+            # Move to next row
+            current_row += 1 if going_down else -1
+
+        # Combine all rows
+        return ''.join(rows)
